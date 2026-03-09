98a9a023c302a2cc4c9db399d7935571b4a69014
nadervika77-netizen <nadervika77@gmail.com>
2026-03-09 15:01:29 -0700

    Update chat.js

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -72,7 +72,7 @@ exports.handler = async (event) => {
     await fetch(`${SUPABASE_URL}/rest/v1/profiles?id=eq.${userData.id}`, {
       method: 'PATCH',
       headers: { Authorization: `Bearer ${SUPABASE_SERVICE_KEY}`, apikey: SUPABASE_SERVICE_KEY, 'Content-Type': 'application/json', Prefer: 'return=minimal' },
-      body: JSON.stringify({ credits: profile.credits - 1, tokens_used: (profile.total_tokens_used || 0) + tokIn + tokOut, total_spent_cents: Math.ceil(thisCost * 10000) / 100 })
+      body: JSON.stringify({ credits: profile.credits - 1, total_tokens_used: (profile.total_tokens_used || 0) + tokIn + tokOut, total_spent_cents: Math.ceil(thisCost * 10000) / 100 })
     });
 
     await fetch(`${SUPABASE_URL}/rest/v1/usage_log`, {
